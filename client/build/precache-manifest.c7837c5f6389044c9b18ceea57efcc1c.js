002f8b8573bd44330935bde4856c9c95af48eef9
parv3213 <parv3213@gmail.com>
2020-07-23 13:14:17 +0530

    added kovan test network

---
+++ client/build/precache-manifest.c7837c5f6389044c9b18ceea57efcc1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce08fc385883840f51994b5b6515148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc73797d642d3457b402",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bc73797d642d3457b402",
+    "url": "/static/js/2.7e266c44.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/static/js/2.7e266c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b476c370e8dfa5e93722",
+    "url": "/static/js/main.8854fd8a.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
